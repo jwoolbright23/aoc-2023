64b2dee804962af343ac9a755a1b8db1d49c329d
John Woolbright <jwoolbright23@gmail.com>
2023-12-04 12:29:07 -0600

    solved part-two

--- puzzle-one.sh
+++ puzzle-one.sh
@@ -18,3 +18,30 @@ calibrate_for_elves () {
 }
 
 calibrate_for_elves
+
+# one, two, three, four, five, six, seven, eight, nine
+
+# Command replacing all existing written numbers using sed to create a recalibrated file
+# sed -E 's/oneight/18/g; s/twone/21/g; s/eightwo/82/g; s/one/1/g; s/two/2/g; s/three/3/g; s/four/4/g; s/five/5/g; s/six/6/g; s/seven/7/g; s/eight/8/g; s/nine/9/g;' input.txt > recalibrated.txt
+
+# Use same logic as part 1 only with new input file
+
+recalibrate_for_the_elves () {
+    recalibrated_array_of_numbers=()
+
+    for line in $(cat recalibrated.txt); do
+        first_number=$(echo $line | sed 's/[^0-9]//g' | cut -c1)
+        last_number=$(echo $line | sed 's/[^0-9]//g' | rev | cut -c1)
+        new_number=$first_number$last_number
+        recalibrated_array_of_numbers+=($new_number)
+    done
+
+    for number in ${recalibrated_array_of_numbers[@]}; do
+        recalibrated_sum=$((recalibrated_sum + number))
+    done
+
+    echo $recalibrated_sum
+
+}
+
+recalibrate_for_the_elves
